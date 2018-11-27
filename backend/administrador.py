962af8aa4eec924c750ef21f18fa0f35c45e2027
kawer <kawer7@gmail.com>
2018-11-26 20:03:38 -0600

    fixed list functions

--- backend/administrador.py
+++ backend/administrador.py
@@ -8,8 +8,12 @@ def taxiList(cursor):
 	result = cursor.fetchone();
 	if result == None:
 		return 'none';
-	if result != None:
-		resultJson = '{ "id":"' + str(result[0]) + '", "nombre":"' + result[1] + '", "fechaDeNacimiento": "' + str(result[2]) + '", "sexo": "' + result[3] + '", "telefono": "' + str(result[4]) + '", "correo": "' + result[5] + '" + }'
+	while result != None:
+		resultString = '{ "id":"' + str(result[0]) + '", "nombre":"' + result[1] + '", "fechaDeNacimiento": "' + str(result[2]) + '", "sexo": "' + result[3] + '", "telefono": "' + str(result[4]) + '", "correo": "' + result[5] + '" + },'
+		result = cursor.fetchone()
+	resultString = resultString[:-1] + ']'
+	return resultString
+
 
 def clienteList(cursor):
 	query = "SELECT * FROM Persona p JOIN Cliente t ON p.id_persona = t.Id_cliente;"
@@ -17,10 +21,24 @@ def clienteList(cursor):
 	result = cursor.fetchone();
 	if result == None:
 		return 'none';
-	if result != None:
-		resultJson = '{ "id":"' + str(result[0]) + '", "nombre":"' + result[1] + '", "fechaDeNacimiento": "' + str(result[2]) + '", "sexo": "' + result[3] + '", "telefono": "' + str(result[4]) + '", "correo": "' + result[5] + '" + }'
+	while result != None:
+		resultJson = '{ "id":"' + str(result[0]) + '", "nombre":"' + result[1] + '", "fechaDeNacimiento": "' + str(result[2]) + '", "sexo": "' + result[3] + '", "telefono": "' + str(result[4]) + '", "correo": "' + result[5] + '" + },'
+	resultString = resultString[:-1] + ']'
+	return resultString
 
 def crearViaje(idCliente, origen, destino, fecha, cursor):
-	query = "UPDATE Persona SET nombre = \"" + nombreN + "\", sexo = \"" + sexoN + "\", correo = \"" + correoN + "\", telefono = \"" + telefonoN + "\" WHERE id_persona = " + idCliente + ";"
-	cursor.execute(query)
+	query = "SELECT ta.Id_taxi FROM Viaje v JOIN Taxi ta ON v.Id_taxi = ta.Id_taxi JOIN Taxista t ON t.id_taxista = ta.id_taxista WHERE t.activo = 1 AND v.Estatus > 1";
+	cursor.execute(query);
+	result = cursor.fetchone();
+	if result == None:
+		return 'none';
+
+	query1 = "SELECT MAX Id_viaje FROM Viaje";
+	cursor.execute(query);
+	idViaje = cursor.fetchone();
+	idViaje++;
+	SELECT FLOOR 
+
+	query2 = "INSERT INTO Viaje VALUES(\"" + idViaje + "\", \"" + idCliente + "\", \"" + fecha + "\", \"" + origen + "\", , \"" + destino + "\",   );"
+	cursor.execute(query2)
 	return "Done"
